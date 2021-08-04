e30055fd90f1cd71056e2abf84adce8a0002d65b
Anurag Borhade <76247619+itsanurag22@users.noreply.github.com>
2021-08-04 20:05:09 +0530

    Update connection.py

--- q3/connection.py
+++ q3/connection.py
@@ -1,5 +1,6 @@
+# establishes connection to sql database "scrape" on localhost of user "anurag22"
 import mysql.connector
 conn = mysql.connector.connect(user='anurag22', password='Password', host='localhost', database='scrape')
-# establishes connection to sql database "scrape" on localhost of user "anurag22"
+
 
 
